9f18ec24584eceb6a489c3ed8367f46eee12ed81
unknown <demophp0205chat>
2020-03-17 20:21:56 +0700

    Updates

---
+++ precache-manifest.b58a9bdd24674383a145117a0235ab5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628b00c49212275b55e5f30d9d099e4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18050a8099424d995cb6",
+    "url": "/static/css/main.2fcfb848.chunk.css"
+  },
+  {
+    "revision": "a9f485b9a1e0883d3776",
+    "url": "/static/js/2.82650afa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.82650afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18050a8099424d995cb6",
+    "url": "/static/js/main.ad7f16aa.chunk.js"
+  },
+  {
+    "revision": "bbba8be9b788b6efe80a",
+    "url": "/static/js/runtime-main.303c2e98.js"
+  }
+]);
